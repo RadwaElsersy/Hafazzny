8a90a16d97df9115741d15b9434792a953aa95c0
RadwaElsersy <radwa.elsersy@usytech.com>
2016-10-06 13:49:45 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hafazzny::Application.config.secret_token = 'bde7b8862afd69402a5a450d0ee7dad485313828a25537e9bb8db0697bf303a98e4c1b02ddd2430878c9961c2e2e570a8e0048899418d3563ce6e6f9b0985f72'
